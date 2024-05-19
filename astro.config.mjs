13061c8f9b727e830ee9c00f5dc50a594c740bb6
Anna Vidal <anna@anna.local>
2024-05-19 14:55:42 +0200

    fix URL in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://annavipers.github.io',
+    site: 'https://annaviper.github.io',
 });
