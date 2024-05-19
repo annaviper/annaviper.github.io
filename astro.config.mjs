3feeb51bb6f1ad35fc549310dc771d982e8affae
Anna Vidal <anna@anna.local>
2024-05-19 15:05:35 +0200

    deploy after build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://annaviper.github.io',
+    output: 'static',
+    outDir: './docs',
+    build: {
+        assets: 'astro'
+    }
 });
